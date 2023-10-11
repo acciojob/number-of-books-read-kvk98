fa322a8e662c22a5ed58ecd44904e18ef2c10c74
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-11 13:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let sum = 0;
+	for (let i = 0; i < library.length; i++) {
+		if (library[i].readingStatus === true) {
+			sum++;
+		}
+	}
+	return sum;
+	
 };
 
 // Do not change the code below
